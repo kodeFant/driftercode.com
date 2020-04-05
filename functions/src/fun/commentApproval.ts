541d2f35a86cd9c4aa10b14be84ddabe96c03875
Lars Lillo Ulvestad <lars.lillo@gmail.com>
2020-04-05 21:31:18 +0200

    Comment approval refactor

--- functions/src/fun/commentApproval.ts
+++ functions/src/fun/commentApproval.ts
@@ -1,8 +1,9 @@
 import { Request, Response } from "express"
 import { Codec, string, GetInterface } from "purify-ts/Codec"
 import { commentsRef } from "../firebase"
-import * as functions from "firebase-functions"
-import fetch from 'cross-fetch';
+
+import { Either } from "purify-ts/Either";
+import { EitherAsync } from "purify-ts/EitherAsync"
 
 const UpdateApprovalBody = Codec.interface({
     commentId: string
@@ -15,47 +16,22 @@ type UpdateApprovalBody = GetInterface<typeof UpdateApprovalBody>
 export default async function commentApproval(request: Request, response: Response) {
 
     const body = { commentId: request.params.commentId }
-    const decodedBody = UpdateApprovalBody.decode(body)
-
-
+    const decodedBody: Either<string, UpdateApprovalBody> = UpdateApprovalBody.decode(body)
 
     if (decodedBody.isRight()) {
-
         const updateApprovalBody = decodedBody.extract()
         const commentRef = await commentsRef.doc(updateApprovalBody.commentId)
         const commentExists: boolean = await commentExistIO(commentRef)
 
-        if (commentExists) {
-            commentRef.update({
-                approved: true
-            })
-                .then(async function () {
-                    const res = await fetch(functions.config().netlify.buidhook, { method: "POST" })
+        const approved = await updateApprovedCommentIO({ commentExists, commentRef, decodedBody }).run()
 
-                    if (res.status >= 400) {
-                        throw new Error("Bad response from server");
-                    }
+        return approved
 
-                    console.log("Comment approval successfully updated!");
-                    response.send("Comment approved")
-
-                    return "Comment approved"
-                })
-                .catch(function (error) {
-                    response.statusCode = 500
-                    return "Something went wrong"
-                });
-        } else {
-            response.statusCode = 500
-            return "Something went wrong"
-        }
-    } else if (decodedBody.isLeft()) {
+    } else {
         response.statusCode = 500
         response.send(decodedBody.extract())
         return `Something went wrong: ${decodedBody.extract()}`
     }
-    console.log("To last return")
-    return "Something went wrong"
 }
 
 async function commentExistIO(commentRef: FirebaseFirestore.DocumentReference<FirebaseFirestore.DocumentData>): Promise<boolean> {
@@ -71,4 +47,26 @@ async function commentExistIO(commentRef: FirebaseFirestore.DocumentReference<Fi
         console.log("Error getting document:", e);
         return false;
     }
+}
+
+
+interface UpdateApprovedCommentIOProps {
+    commentExists: boolean,
+    commentRef: FirebaseFirestore.DocumentReference<FirebaseFirestore.DocumentData>,
+    decodedBody: Either<string, UpdateApprovalBody>
+}
+
+
+
+function updateApprovedCommentIO({ commentExists, commentRef, decodedBody }: UpdateApprovedCommentIOProps): EitherAsync<string, string> {
+    const updatedApprovedComment = EitherAsync<string, string>(async ({ liftEither, fromPromise }) => {
+        if (commentExists) {
+            await commentRef.update({ approved: true })
+            return ("Comment approved")
+        } else {
+            return ("There was an error when updating comment")
+        }
+    })
+    return updatedApprovedComment
+
 }
