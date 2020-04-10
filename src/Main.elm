a77dec77358badaaa46686cc58786ad4eaac1356
Lars Lillo Ulvestad <lars.lillo@gmail.com>
2020-04-10 12:46:56 +0200

    Remove trailing slash on canonical site url

--- src/Main.elm
+++ src/Main.elm
@@ -21,7 +21,7 @@ import Webmanifest exposing (manifest)
 
 canonicalSiteUrl : String
 canonicalSiteUrl =
-    "https://driftercode.com/"
+    "https://driftercode.com"
 
 
 main : Pages.Platform.Program Model Msg Metadata (Rendered Msg)
